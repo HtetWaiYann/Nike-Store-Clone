02362b16b5a118c8c34d64c8a961bfd830749395
Htet Wai Yan <hw.yan@mit.com.mm>
2024-02-10 22:27:52 +0630

    add style for items

--- src/components/utils/Item.tsx
+++ src/components/utils/Item.tsx
@@ -3,30 +3,44 @@ import { Item } from "../../interface/sales";
 
 const Item = ({ item }: { item: Item }) => {
   return (
-    <div className="">
-      <div className="">
-        <h1 className="">{item.title}</h1>
-        <p>{item.text}</p>
-        <div className="">
-          <div className="">
-            <h1 className="">{item.price}</h1>
+    <div
+      className={`relative bg-gradient-to-b ${item.shadow} ${item.color} grid items-center justify-items-center`}
+    >
+      <div className="grid items-center justify-items-center">
+        <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">
+          {item.title}
+        </h1>
+        <p className="text-slate-200 filter drop-shadow text-base md:text-sm font-normal">
+          {item.text}
+        </p>
+        <div className="flex items-center justify-between w-28 my-2">
+          <div className="flex items-center bg-white/80  px-1 rounded blur-effect-theme">
+            <h1 className="text-black text-sm font-medium">${item.price}</h1>
           </div>
-          <div className="">
-            <StarIcon className="icon-style text-slate-900" />
-            <h1 className="">{item.rating}</h1>
+          <div className="flex items-center gap-1">
+            <StarIcon className="icon-style w-5 h-5 md:w-4 md:h-4" />
+            <h1 className="md:text-sm font-normal text-slate-100">
+              {item.rating}
+            </h1>
           </div>
         </div>
-        <div className="">
-          <button type="button" className="">
+        <div className="flex items-center gap-3">
+          <button
+            type="button"
+            className="bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200"
+          >
             <ShoppingBagIcon className="icon-style text-slate-900" />
           </button>
-          <button type="button" className="">
+          <button
+            type="button"
+            className="bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black"
+          >
             {item.btn}
           </button>
         </div>
       </div>
-      <div className="">
-        <img src={item.img} alt="img/item" className=""/>
+      <div className="flex items-center">
+        <img src={item.img} alt="img/item" className="" />
       </div>
     </div>
   );
